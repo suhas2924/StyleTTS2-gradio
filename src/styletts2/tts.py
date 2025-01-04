54ab6bfc4e69830566717e6c52fa9afd65ec839e
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-01-04 10:11:09 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -79,9 +79,9 @@ def preprocess_to_ignore_quotes(text):
     text = re.sub(r'[ \t]+', ' ', text)  # Collapsing multiple spaces/tabs into one
     return text
 
-def segment_text(text, min_chars=90, max_chars=120):
+def segment_text(text, min_chars=170, max_chars=200):
     # Split the text by punctuation while retaining the delimiters
-    sentences = re.split(r'(\.\.\."?|[.,!?]"?)', text)
+    sentences = re.split(r'(\.\.\."?|[.,?]"?)', text)
     sentences = [''.join(i).strip() for i in zip(sentences[0::2], sentences[1::2])]
 
     batches = []
@@ -375,6 +375,10 @@ class StyleTTS2:
         text = preprocess_to_ignore_quotes(text)
     
         text_segments = segment_text(text)
+
+        # Replace only the last period or comma at the end of the segment with ellipsis
+        text_segments = [re.sub(r'([.,])(?=["\s]*["]?$)', '...', text_segment) for text_segment in text_segments]
+    
         segments = []
         prev_s = None
         for text_segment in text_segments:
