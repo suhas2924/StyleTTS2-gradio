6ed41ca4d7787356eaff7ae2a4d6d71872ab7723
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-01-14 03:56:05 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -1,7 +1,3 @@
-import nltk
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-
 from pathlib import Path
 import librosa
 import scipy
@@ -291,7 +287,7 @@ class StyleTTS2:
 
             # encode prosody
             en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(self.device))
-            if self.model_params.decoder.type == "bigvgan":
+            if self.model_params.decoder.type == "hifigan":
                 asr_new = torch.zeros_like(en)
                 asr_new[:, :, 0] = en[:, :, 0]
                 asr_new[:, :, 1:] = en[:, :, 0:-1]
@@ -300,7 +296,7 @@ class StyleTTS2:
             F0_pred, N_pred = self.model.predictor.F0Ntrain(en, s)
 
             asr = (t_en @ pred_aln_trg.unsqueeze(0).to(self.device))
-            if self.model_params.decoder.type == "bigvgan":
+            if self.model_params.decoder.type == "hifigan":
                 asr_new = torch.zeros_like(asr)
                 asr_new[:, :, 0] = asr[:, :, 0]
                 asr_new[:, :, 1:] = asr[:, :, 0:-1]
@@ -448,7 +444,7 @@ class StyleTTS2:
 
             # encode prosody
             en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(self.device))
-            if self.model_params.decoder.type == "bigvgan":
+            if self.model_params.decoder.type == "hifigan":
                 asr_new = torch.zeros_like(en)
                 asr_new[:, :, 0] = en[:, :, 0]
                 asr_new[:, :, 1:] = en[:, :, 0:-1]
@@ -457,7 +453,7 @@ class StyleTTS2:
             F0_pred, N_pred = self.model.predictor.F0Ntrain(en, s)
 
             asr = (t_en @ pred_aln_trg.unsqueeze(0).to(self.device))
-            if self.model_params.decoder.type == "bigvgan":
+            if self.model_params.decoder.type == "hifigan":
                 asr_new = torch.zeros_like(asr)
                 asr_new[:, :, 0] = asr[:, :, 0]
                 asr_new[:, :, 1:] = asr[:, :, 0:-1]
