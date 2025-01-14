848aaad11bcd549e84ad61a551ce07705e6d5979
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-01-15 01:29:46 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -348,7 +348,6 @@ class StyleTTS2:
         text = preprocess_to_ignore_quotes(text)
     
         text_segments = segment_text(text)
-        text_segments = [re.sub(r'([.,])(?=["\s]*["]?$)', '…', text_segment) for text_segment in text_segments]
         
         segments = []
         prev_s = None
