d098225b1da82c881977a713d2a6add383174e53
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-01-30 18:28:07 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -79,13 +79,14 @@ def preprocess_to_ignore_quotes(text):
     return text
 
 def segment_text(text, max_length=300):
+    """Splits text into segments of max_length while preserving sentence structure."""
     doc = nlp(text)
-
     segments = []
     current_segment = ""
-    
+
     for sentence in doc.sents:
         sentence_text = sentence.text.strip()
+
         # Check if adding this sentence exceeds the max length
         if len(current_segment) + len(sentence_text) + 1 <= max_length:
             current_segment += " " + sentence_text if current_segment else sentence_text
@@ -253,6 +254,7 @@ class StyleTTS2:
             ref_s = self.compute_style(target_voice_path)  # target style vector
 
         text = text.strip()
+        text = text.replace('.', '...')
         text = text.replace('…', '...')
         phonemized_text = global_phonemizer.phonemize([text]) 
         phoneme_string = ' '.join(phonemized_text).strip()
@@ -361,7 +363,6 @@ class StyleTTS2:
         # Preprocess the text (e.g., clean up quotes and spaces)
         text = preprocess_to_ignore_quotes(text)
         text_segments = segment_text(text)
-        text_segments = [re.sub(r'([.])(?=["\s]*["]?$)', '...', text_segment) for text_segment in text_segments]
         
         segments = []
         prev_s = None
@@ -404,6 +405,7 @@ class StyleTTS2:
         :return: audio data as a Numpy array
         """
         text = text.strip()
+        text = text.replace('.', '...')
         text = text.replace('…', '...')
         phonemized_text = global_phonemizer.phonemize([text])
         phoneme_string = ' '.join(phonemized_text).strip()
