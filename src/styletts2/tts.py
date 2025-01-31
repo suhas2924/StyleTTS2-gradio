a722d5dff3498577f1972d3cbe1d1080228f450e
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-01-31 10:53:57 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -254,7 +254,6 @@ class StyleTTS2:
             ref_s = self.compute_style(target_voice_path)  # target style vector
 
         text = text.strip()
-        text = text.replace('.', '...')
         text = text.replace('…', '...')
         phonemized_text = global_phonemizer.phonemize([text]) 
         phoneme_string = ' '.join(phonemized_text).strip()
@@ -405,7 +404,6 @@ class StyleTTS2:
         :return: audio data as a Numpy array
         """
         text = text.strip()
-        text = text.replace('.', '...')
         text = text.replace('…', '...')
         phonemized_text = global_phonemizer.phonemize([text])
         phoneme_string = ' '.join(phonemized_text).strip()
