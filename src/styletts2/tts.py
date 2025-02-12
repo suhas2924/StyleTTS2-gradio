d760be94db2226790710160cd9bc51dc1c0eeb92
suhas2924 <42748911+suhas2924@users.noreply.github.com>
2025-02-13 01:43:23 +0530

    tts.py

--- src/styletts2/tts.py
+++ src/styletts2/tts.py
@@ -196,24 +196,25 @@ class StyleTTS2:
 
         return model
 
-    def compute_style(path: str) -> torch.Tensor:
+    def compute_style(self, path) -> torch.Tensor:
         """
-        Load an audio file, trim it, resample if needed, then
-        compute and return a style vector by passing through the style encoder
-        and predictor encoder.
+        Compute style vector, essentially an embedding that captures the characteristics
+        of the target voice that is being cloned
+        :param path: Path to target voice audio file
+        :return: style vector
         """
         wave, sr = librosa.load(path, sr=24000)
-        audio, _ = librosa.effects.trim(wave, top_db=40)
+        audio, index = librosa.effects.trim(wave, top_db=40)
         if sr != 24000:
             audio = librosa.resample(audio, sr, 24000)
+        mel_tensor = preprocess(audio).to(self.device)
 
-        mel_tensor = preprocess(audio).to(device)
         with torch.no_grad():
-            ref_s = model.style_encoder(mel_tensor.unsqueeze(1))
-            ref_p = model.predictor_encoder(mel_tensor.unsqueeze(1))
+            ref_s = self.model.style_encoder(mel_tensor.unsqueeze(1))
+            ref_p = self.model.predictor_encoder(mel_tensor.unsqueeze(1))
 
         return torch.cat([ref_s, ref_p], dim=1)
-    
+
     def inference(self,
                   text: str,
                   target_voice_path=None,
